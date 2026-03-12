c9e5169917da54c6a8c72f68a9034c398e036864
nikoladzomic <142274744+nikoladzomic@users.noreply.github.com>
2026-03-12 20:48:45 +0100

    Refactor Program.cs to include Razor Components

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-builder.Services.AddScoped<IProizvodRepository, ProizvodRepository>();
-builder.Services.AddScoped<IBusiness, Business>();
+using BusinessLayer;
+using DataLayer;
+using Web.Components;
+
+namespace Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            builder.Services.AddScoped<IProizvodRepository, ProizvodRepository>();
+            builder.Services.AddScoped<IBusiness, Business>();
+
+            var app = builder.Build();
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
